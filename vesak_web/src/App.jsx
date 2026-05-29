456bce9d8ccee3d84f7e598faeee7765d15bd979
dilshansethmina2002 <132134098+dilshansethmina2002@users.noreply.github.com>
2026-05-29 11:39:44 +0530

    feat: Add Vercel Analytics for improved tracking; update branding to use 'සඳකඩ'

--- vesak_web/src/App.jsx
+++ vesak_web/src/App.jsx
@@ -7,6 +7,7 @@ import CreatePost from './pages/CreatePost';
 import ViewCard from './pages/ViewCard';
 import VesakZone from "./pages/VesakZone";
 import AdminDashboard from "./pages/AdminDashboard";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -22,7 +23,10 @@ function App() {
           <Route path="card/:id" element={<ViewCard />} />
         </Route>
       </Routes>
+      <Analytics />
     </BrowserRouter>
+    
+
   );
 }
 
